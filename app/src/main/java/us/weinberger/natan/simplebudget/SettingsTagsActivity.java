12b9a91e408ec1239a9b0333698ca72a51cf5551
Natan Weinberger <naweinberger@gmail.com>
2014-06-22 22:04:01 -0400

    added DragSortListView

--- app/src/main/java/us/weinberger/natan/simplebudget/SettingsTagsActivity.java
+++ app/src/main/java/us/weinberger/natan/simplebudget/SettingsTagsActivity.java
@@ -4,16 +4,83 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.mobeta.android.dslv.DragSortController;
+import com.mobeta.android.dslv.DragSortListView;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by Natan on 6/22/2014.
  */
 public class SettingsTagsActivity extends Activity {
+    DragSortListView listView;
+    ArrayAdapter<String> adapter;
 
+    private DragSortListView.DropListener onDrop = new DragSortListView.DropListener()
+    {
+        @Override
+        public void drop(int from, int to)
+        {
+            if (from != to)
+            {
+                String item = adapter.getItem(from);
+                adapter.remove(item);
+                adapter.insert(item, to);
+            }
+        }
+    };
+
+    private DragSortListView.RemoveListener onRemove = new DragSortListView.RemoveListener()
+    {
+        @Override
+        public void remove(int which)
+        {
+            adapter.remove(adapter.getItem(which));
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_tags);
+
+        listView = (DragSortListView) findViewById(R.id.listview);
+        String[] names = new String[]{"0", "1"};
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(names));
+        adapter = new ArrayAdapter<String>(this,
+                R.layout.list_row_layout, R.id.amount, list);
+        listView.setAdapter(adapter);
+        listView.setDropListener(onDrop);
+        listView.setRemoveListener(onRemove);
+
+        DragSortController controller = new DragSortController(listView);
+        //controller.setDragHandleId(R.id.imageView1);
+        //controller.setClickRemoveId(R.id.);
+        controller.setRemoveEnabled(false);
+        controller.setSortEnabled(true);
+        controller.setDragInitMode(1);
+        //controller.setRemoveMode(removeMode);
+
+        listView.setFloatViewManager(controller);
+        listView.setOnTouchListener(controller);
+        listView.setDragEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 }
